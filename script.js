334c99d30570d306362c4dae46169df3a052e750
Praveenk-joe <praveenkumar16615@gmail.com>
2025-03-11 17:30:16 +0530

    trave

---
+++ script.js
@@ -0,0 +1,10 @@
+function showSidebar() {
+    const show = document.querySelector('#ul');
+    show.style.display = 'block'
+
+}
+
+function hideSidebar() {
+    const show = document.querySelector('#ul');
+    show.style.display = 'none'
+}
